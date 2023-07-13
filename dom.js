f5143f97a70c078564189f5a878b02cc4546f48b
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-07-13 23:23:00 +0530

    QuerySelector

--- dom.js
+++ dom.js
@@ -31,17 +31,51 @@
 
 //GET ELEMENTS BY TAG NAME//
 
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent='Hello 2';
-li[1].style.fontWeight = 'bold';
-li[4].style.backgroundColor = 'green';
+//console.log(li);
+//console.log(li[1]);
+//var li = document.getElementsByTagName('li');
+//li[1].textContent='Hello 2';
+//li[1].style.fontWeight = 'bold';
+//li[4].style.backgroundColor = 'green';
 
 
 //for(var i = 0;i<li.length;i++){
 //    li[i].style.backgroundColor = 'yellow';
 //}
 
-v//ar lister = document.getElementsByClassName('Lister');
+//ar lister = document.getElementsByClassName('Lister');
 //lister.style.backgroundColor = 'red';
+
+// QUERYSELECTOR //
+
+//var header = document.querySelector('#main-header');
+//header.style.borderBottom = 'solid 4 px #ccc';
+
+//var input = document.querySelector('input');
+//input.value = 'Hello World';
+
+//var submit = document.querySelector('input[type="submit"]');
+//submit.value='SEND' 
+
+//var secondItem = document.querySelector
+//('.list-group-item:nth-child(2)');
+//secondItem.style.backgroundColor = 'green';
+
+//var secondItem = document.querySelector
+//('.list-group-item:nth-child(3)');
+//secondItem.style.color = 'white';
+
+// QUERYSELECTORALL //
+
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i = 0; i < odd.length; i++){
+   odd[i].style.backgroundColor = '#f4f4f4'; 
+   even[i].style.backgroundColor = '#ccc';
+}
