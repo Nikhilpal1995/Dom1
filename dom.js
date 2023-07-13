dd8e93edfd461117d98fff9573f834d8d0b7ca88
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-07-13 08:28:49 +0530

    dom1

---
+++ dom.js
@@ -0,0 +1,16 @@
+//Examine the document object 
+
+//console.dir(document);
+
+//console.log(document.documentURI)
+
+//GET ELEMENT BY ID//
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent = 'Hello'
+//headerTitle.innerText = 'GoodBye'
+//console.log(headerTitle.innerText);
+header.style.borderBottom = 'solid 3px #000'
+
