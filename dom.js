54b7e1e9b8f5190ea284b0e2cd15a4beb0fee451
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-07-13 21:51:48 +0530

    Get Element by Class Name

--- dom.js
+++ dom.js
@@ -6,11 +6,25 @@
 
 //GET ELEMENT BY ID//
 
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
 //console.log(headerTitle);
 //headerTitle.textContent = 'Hello'
 //headerTitle.innerText = 'GoodBye'
 //console.log(headerTitle.innerText);
-header.style.borderBottom = 'solid 3px #000'
+//header.style.borderBottom = 'solid 3px #000'
 
+//GET ELEMENT BY CLASS NAME//
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='Hello 2';
+items[1].style.fontWeight = 'bold';
+//items[1].style.backgroundColor = 'yellow';
+
+//items.style.backgroundColor = 'f4f4f4';
+
+for(var i = 0;i<items.length;i++){
+    items[i].style.backgroundColor = '#f4f4f4';
+}
