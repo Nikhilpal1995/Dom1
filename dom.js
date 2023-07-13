c16e8747f2b15e68b23c789051b4a54181d0939b
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-07-13 22:18:27 +0530

    Get Elements by Tag Name

--- dom.js
+++ dom.js
@@ -14,17 +14,34 @@
 //console.log(headerTitle.innerText);
 //header.style.borderBottom = 'solid 3px #000'
 
-//GET ELEMENT BY CLASS NAME//
+//GET ELEMENTS BY CLASS NAME//
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='Hello 2';
-items[1].style.fontWeight = 'bold';
-//items[1].style.backgroundColor = 'yellow';
+//var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent='Hello 2';
+//items[1].style.fontWeight = 'bold';
+//items[2].style.backgroundColor = 'green';
 
 //items.style.backgroundColor = 'f4f4f4';
 
-for(var i = 0;i<items.length;i++){
-    items[i].style.backgroundColor = '#f4f4f4';
-}
+//for(var i = 0;i<items.length;i++){
+//    items[i].style.fontWeight = 'bold';
+//}
+
+//GET ELEMENTS BY TAG NAME//
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='Hello 2';
+li[1].style.fontWeight = 'bold';
+li[4].style.backgroundColor = 'green';
+
+
+//for(var i = 0;i<li.length;i++){
+//    li[i].style.backgroundColor = 'yellow';
+//}
+
+v//ar lister = document.getElementsByClassName('Lister');
+//lister.style.backgroundColor = 'red';
